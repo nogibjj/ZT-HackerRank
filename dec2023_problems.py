690a82a07bcb722a21579928f2eb8f488fb3d24e
JTuratkhan <108810678+JTuratkhan@users.noreply.github.com>
2023-12-22 23:45:53 +0000

    rearrange a word done

--- dec2023_problems.py
+++ dec2023_problems.py
@@ -26,5 +26,38 @@ def breakPalindrome(palindromeStr):
 # palindromeStr = 'aaabaaa'
 # print(breakPalindrome(palindromeStr))
 
-palindromeStr = 'mom'
-print(breakPalindrome(palindromeStr))
+# palindromeStr = 'mom'
+# print(breakPalindrome(palindromeStr))
+
+
+
+
+""" PROBLEM 2 - Rearranging a Word """
+
+
+def rearrangeWord(word):
+    # Find non-increasing suffix
+    i = len(word) - 1
+    while i > 0 and word[i-1] >= word[i]:
+        i -= 1
+    if i == 0: # Word is in the last permutation
+        return 'no answer'
+
+    # Find successor to pivot
+    j = len(word) - 1
+    while word[j] <= word[i - 1]:
+        j -= 1
+
+    word_list = list(word)
+
+    # Swap the pivot with j
+    word_list[i - 1], word_list[j] = word_list[j], word_list[i - 1]
+
+    # Reverse the suffix
+    word_list[i:] = word_list[len(word) - 1 : i - 1 : -1]
+
+    return ''.join(word_list)
+
+# Example usage:
+word = 'xy'
+print(rearrangeWord(word))
